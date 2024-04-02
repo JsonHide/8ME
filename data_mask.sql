e08afa734345bc5c429d0be2cf22caa0ac8a7497
JsonHide <97667747+JsonHide@users.noreply.github.com>
2024-04-02 15:10:33 +0100

    Update data_mask.sql

--- data_mask.sql
+++ data_mask.sql
@@ -1,3 +1,4 @@
 go
 --
-UPDATE strategy SET StrategyName = 'ÉÌ»ú' +  CONVERT(varchar,StrategyId) WHERE ObjType='Strategy';
+UPDATE strategy SET StrategyName = '8M-Strategy' +  CONVERT(varchar,StrategyId) WHERE ObjType='Strategy';
+UPDATE strategy SET StrategyName = '8M-deal' +  CONVERT(varchar,StrategyId) WHERE ObjType='Deal';
